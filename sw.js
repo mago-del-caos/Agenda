b39b40a6db0a3cf28bef64b53e37ff8354d8856e
mago-del-caos <profesorxjuvenal@gmail.com>
2026-08-17 08:56:23 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nombre y versión de la caché (Sube este número en cada actualización)
-const APP_VERSION = 'aglucem-v37';
+const APP_VERSION = 'aglucem-v38';
 
 // Lista de archivos que la PWA guardará en la memoria del celular
 const ASSETS_TO_CACHE = [
