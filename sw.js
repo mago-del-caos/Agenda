705d328274c47516e92fbf1850e788d7ec209af4
mago-del-caos <profesorxjuvenal@gmail.com>
2026-08-26 08:54:20 -0600

    Update cache version to v49

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Nombre y versión de la caché (Subido a v48 para forzar el cambio)
-const APP_VERSION = 'aglucem-v48';
+// Nombre y versión de la caché (Subido a v49 para forzar el cambio)
+const APP_VERSION = 'aglucem-v49';
 
 // Lista de archivos que la PWA guardará en la memoria del celular
 // (Se eliminaron las imágenes de Novedades porque ahora viven en Neocities)
