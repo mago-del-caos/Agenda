fcf1c8b132c84f89d357391a54463b6e9f55a547
mago-del-caos <profesorxjuvenal@gmail.com>
2026-08-26 09:47:59 -0600

    Update cache version to v61

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Nombre y versión de la caché (Actualizado a v60 para forzar el cambio)
-const APP_VERSION = 'aglucem-v60';
+// Nombre y versión de la caché (Actualizado a v61 para forzar el cambio)
+const APP_VERSION = 'aglucem-v61';
 
 // Lista de archivos que la PWA guardará en la memoria del celular
 const ASSETS_TO_CACHE = [
